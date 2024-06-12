c2f257d8e25158ada660391c8b29da09da4d5e83
maulanasidiq <lbisa4896@gmail.com>
2024-06-12 09:33:08 +0800

    Initial commit

---
+++ profil.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/sidiq.jpeg" alt="Muhammad Maulana Sidiq" width="120" class="rounded-circle">
+            <table class="table mt-4" style="width: 100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2210010175</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Maulana Sidiq</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Palangka Raya, 28 Oktober 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">087735498693</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">maulanasidiq86@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/mdedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/facebook.jpg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/instagram.jpg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/twiter.png" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/dedyrosyadi" target="_blank">
+                                <img src="linkedin.png" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
